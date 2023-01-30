2c7da5c342826d2d5231dd9ea971f68639612c51
Samuel E. Conrado <samuelconrado59@gmail.com>
2023-01-30 13:01:19 -0300

    projeto_3

--- src/main/java/model/Line.java
+++ src/main/java/model/Line.java
@@ -1,11 +1,28 @@
 package model;
 
-public enum Line {
-    CRONOS("Cronos"),
-    ARES(  "Ares");
 
-    private final String name;
-    Line(String name) {
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+@Entity
+@Table(name = "line_table")
+public class Line {
+//    CRONOS("Cronos"),
+//    ARES(  "Ares");
+
+    @Id
+
+    @Column(name = "name", columnDefinition = "text")
+    private String name;
+
+    public Line() {
+    }
+
+    public Line(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
