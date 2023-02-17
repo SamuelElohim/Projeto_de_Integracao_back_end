38e5762178f7dc4dc7fb237cb1fab8f60ba515f0
Samuel E. Conrado <samuelconrado59@gmail.com>
2023-02-17 16:58:12 -0300

    refactor classes 'HibernateSession', 'HibernateSessionFactory' to implement Singleton pattern

--- src/main/java/model/LineEntity.java
+++ src/main/java/model/LineEntity.java
@@ -5,14 +5,13 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "line_table")
-public class LineEntity {
+public class LineEntity implements EntityInterface {
 
     @Id
     @Column(name = "name", columnDefinition = "text")
     private String name;
 
-    public LineEntity() {
-    }
+    public LineEntity() {}
 
     public LineEntity(String name) {
         this.name = name;
